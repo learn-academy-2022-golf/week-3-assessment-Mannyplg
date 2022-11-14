494bbcbd65d2d20f5670ce0b04345cd022a41e48
Mannyplg <pettigrew.i@gmail.com>
2022-11-14 04:11:16 -0800

    Unsuccessful Jest Test, Inc...completed other two...

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,31 +1,100 @@
-// ASSESSMENT 3: Coding Practical Questions with Jest
+// // ASSESSMENT 3: Coding Practical Questions with Jest
+// Immanuel Golf 2022
 
-// Please read all questions thoroughly
-// Pseudo coding is REQUIRED
-// If you get stuck, please leave comments to help us understand your thought process
+// const { defaults } = require("jest-config")
 
-// Use test driven development to complete the following questions
-// Add appropriate dependencies: $ yarn add jest
+// // Please read all questions thoroughly
+// // Pseudo coding is REQUIRED
+// // If you get stuck, please leave comments to help us understand your thought process
 
-// Reminder: The test will call your function
-// Run the file with the following command: $ yarn jest
+// // Use test driven development to complete the following questions
+// // Add appropriate dependencies: $ yarn add jest
 
-// --------------------1) Create a function that takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence.
+// // Reminder: The test will call your function
+// // Run the file with the following command: $ yarn jest
 
-// a) Create a test with expect statements for each of the variables provided.
+// // --------------------1) Create a function that takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence.
 
-const fibLength1 = 6
-// Expected output: [1, 1, 2, 3, 5, 8]
+// // a) Create a test with expect statements for each of the variables provided.
 
-const fibLength2 = 10
-// Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+// // describe("greeter", () => {
+// //   it("returns a generic greeting", () => {
+// //     expect(greeter()).toEqual("Hello, LEARN student!")
+// //   })
+// // })
 
-// b) Create the function that makes the test pass.
+
+// describe("fibonacci", () => {
+//   it("returns an array that length containing the numbers of the Fibonacci sequence", () => {
+//     expect(fibonacci(fibLength1)).toEqual([1, 1, 2, 3, 5, 8])
+//   })
+// })
+
+//  const fibLength1 = 6
+// // Expected output: [1, 1, 2, 3, 5, 8]
+
+// const fibLength2 = 10
+// // Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+
+// // 1st Attempt:
+// //  TypeError: fibLength1 is not a function
+// //  Suites: 1 failed, 1 total
+// //  Tests:       1 failed, 1 total
+
+// // 2nd Attempt:
+// // ReferenceError: fib is not defined
+// // Test Suites: 1 failed, 1 total
+// // Tests:       1 failed, 1 total
+
+// // b) Create the function that makes the test pass.
+
+  // function fibonacci () {
+  //     this.looping = function() {
+  //       var a = 0, b = 1, f = 1
+  //       for(var i = 2; i <= navigator; i++) {
+  //         f = a + b
+  //         a = b
+  //         b = f
+  //       }
+  //       return f
+  //     };
+
+    //   this.recursive = function(n) {
+    //     if (n - 2) {
+    //       return 1
+    //     }else {
+    //       return this.recursive(n - 1) + this.recursive(n -2)
+    //     }
+    //   };
+    // }
+
+//   const fibArr = [0, 1]
+
+//   for (i = 0; i < fibArr.length; 1++) {
+//     if (i > 1) {
+//       const a = fibArr[(i -1)]
+//       const b = fibArr[(i - 2)]
+//       fibArr.push(a + b)
+//     } else {
+//       fibArr[i]
+//     }
+//   } 
+//   return fibArr
+//  }
+//  console.log(fibonacci(10))
+
+// There were several Fibonacci code snippets to select and run a Jest test with. I chose this example for several reasons. A class was created for the function. The keyword this and dot notation was use to to access the function class. Also show the differences in use cases of the for ...loop and recursion. Recursion is a function method that call on it self until it met condition or not needed. A for...loop is a control that wll execute a set of instructions until condition is true. AStill unable to create a successful test.
+
 
 // --------------------2) Create a function that takes in an object and returns an array of the values sorted from least to greatest.
 // Hint: Check out this resource: Object.values() https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_objects/Object/values
 
 // a) Create a test with expect statements for each of the variables provided.
+// describe("goya", () => {
+//   it("returns an array of the values sorted from least to greatest.", () => {
+//     expect(goya(studyMinutesWeek1)).toEqual([15, 15, 20, 30, 30, 60, 90])
+//   })
+// })
 
 const studyMinutesWeek1 = {
   sunday: 90,
@@ -50,10 +119,28 @@ const studyMinutesWeek2 = {
 // Expected output: [10, 15, 20, 45, 60, 65, 100]
 
 // b) Create the function that makes the test pass.
+// ß// Test Suites: 1 failed, 1 total
+// Tests:       1 failed, 1 total
+// ReferenceError: goya is not defined
+
+const goya = () => {
+ return Object.values.sort((a, b) => a - b )
+}
 
 // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
 
 // a) Create a test with expect statements for each of the variables provided.
+
+// // describe("greeter", () => {
+// //   it("returns a generic greeting", () => {
+// //     expect(greeter()).toEqual("Hello, LEARN student!")
+// //   })
+// })
+describe("aSum", () => {
+  it("returns an array of the accumulating sum", () => {
+    expect(aSum(accountTransactions1)).toEqual(100, 83, 60, 51)
+  })
+})
 
 const accountTransactions1 = [100, -17, -23, -9]
 // Expected output: [100, 83, 60, 51]
@@ -65,3 +152,10 @@ const accountTransactions3 = []
 // Expected output: []
 
 // b) Create the function that makes the test pass.
+
+// Test Suites: 1 failed, 1 total - 1st Att.
+// Tests:       1 failed, 1 total
+
+// const aSum = (arr) => {
+//   return sum => value => sum += value
+// }
--- interview-questions.md
+++ interview-questions.md
@@ -1,4 +1,5 @@
 # ASSESSMENT 3: Interview Practice Questions
+Immanuel Golf 2022
 
 Answer the following questions.
 
@@ -8,39 +9,38 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is JSX? What is one syntax difference between HTML and JSX?
 
-Your answer:
+Your answer: JSX is an Javascript translator for HTML. Any code written in JavaScript, JSX translates those commands so HTML understand and output to our screens. 
 
-Researched answer:
+Researched answer: JSX stands for JavaScript XML, it allows developers to write HMTL in React and converts that into React components. React is a JavaAcript library, which allow developers to create applications by combining small components to form one complete working program (i.e. Voltron; was formed by five independent lions with their own unique abilities; when called (invoked) to perform together, they formed Voltron). 
 
 2. What is yarn? What file(s) are modified in a React application when you run the command yarn in your terminal?
 
-Your answer:
+Your answer: Yarn is an dependency package manager. A package manager serves many duties. One is, configuring our computers or updating software in order to run our code. Similiarly to node package manager; yarn provides behind the scene packages to run our programs. 
 
-Researched answer:
+Researched answer: Yarn is a package manager for developers code and allow developers to share their code with others worldwide. 
 
 3. What is an anonymous function in JavaScript?
 
-Your answer:
+Your answer: An anonymous function, is a function this is trigger once invoke. Like console.log()...Once a argument is passed through, we expected to see an printed output in the console. 
 
-Researched answer:
+Researched answer: Anonymous functions are nameless functions, that can be passed as agruments to other functions.
 
 4. What is the difference between state values and props in React?
 
-Your answer:
+Your answer: State values in React can be changed and updated in real time. Properties in React are used to pass information through components of and application. That information updates the state of our application.  
 
-Researched answer:
+Researched answer: Data passed through components is done using props. Statecannot be passed from component to other components. State is only available to that component.
 
 5. What is the DOM?
+Your answer: The DOM is the computer. Everything we see on the screen to include program, are all accessible by DOM. DOM is the parent of all programs inside our computers, which us to program, which gives us access to unlimited uses
 
-Your answer:
-
-Researched answer:
+Researched answer: The DOM allows programmers to directly interact with the interface of the web page through nodes and elements in the DOM. JavaScript is one langauage that can make changes to a web page by communicating to the DOM. 
 
 6. STRETCH: Which is the difference between a div and a span?
 
-Your answer:
+Your answer: One difference...the span is only affecting an assigned element on the page. A div governs itself. It be place anywhere or nowhere. A div can hold huge amounts of data with elements. Span simply has one job.
 
-Researched answer:
+Researched answer: A span element is used for inline styling of that specific element. A div element is used to style a whole page. 
 
 ## Looking Ahead: Terms for Next Week
 
